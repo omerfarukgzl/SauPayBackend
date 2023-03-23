1c2a33a16e64430dfb25e83f433f33c1a24a385c
OMER FARUK GUZEL <58605364+omerfarukgzl@users.noreply.github.com>
2023-03-23 16:02:21 +0300

    update prop

--- SauPay-Payment/domain-service/src/main/java/com/saupay/domainservice/DomainServiceApplication.java
+++ SauPay-Payment/domain-service/src/main/java/com/saupay/domainservice/DomainServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableEurekaClient
+@EnableFeignClients // Feign Client olmak zorunda. Domain diğer servisleri çağırmak için Feign Client olması gerekiyor.
+@EnableEurekaClient// Eureka Client olmak zorunda. Gateway loadbalance yaparken Eureka Client olması gerekiyor.
 public class DomainServiceApplication {
 
 	public static void main(String[] args) {
